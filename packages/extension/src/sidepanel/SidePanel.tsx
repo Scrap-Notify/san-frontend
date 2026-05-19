e5d2b6ac3c4093eed1714068798a3c37e3694927
yejij00ngji <hyi704336@gmail.com>
2026-05-19 15:30:35 +0900

    style(ui): 지식카드 생성 시 상세보기로 리다이렉트 (S14P31A309-290)

--- packages/extension/src/sidepanel/SidePanel.tsx
+++ packages/extension/src/sidepanel/SidePanel.tsx
@@ -921,7 +921,7 @@ export default function SidePanel() {
           {/* 2. Creation Result Area (Fixed 120px): Appears only after successful creation */}
               {isAuthenticated && createdCard && !hasKnowledgeSearchResult && (
                 <div className="shrink-0">
-                  <CreatedKnowledgeCard card={createdCard} />
+                  <CreatedKnowledgeCard card={createdCard} onOpenCard={openDashboardCardDetail} />
                 </div>
               )}
 
